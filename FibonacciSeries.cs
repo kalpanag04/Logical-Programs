b08cd5f29ca7f7aa5daf7d7b423b5fff06365ca0
kalpanag04 <kalpanag808@gmail.com>
2022-07-26 21:50:52 +0530

    Fibonacci Series Program

---
+++ FibonacciSeries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public int Fibonacciseries(int range)
+        {
+            //variables
+            int fibonacci, previous_number = 1, current_number = previous_number + 1;
+            //check if number is greater than one
+            if (range <= 1)
+            {
+                Console.WriteLine("enter the range greater than 1.");
+                return 0;
+            }
+            //display initial numbers 1 and 2.
+            Console.Write("Fibonacci Series :" + previous_number + " " + current_number);
+            for (int counter = 1; counter <= range; counter++)
+            {
+                fibonacci = previous_number + current_number;
+                previous_number = current_number;
+                current_number = fibonacci;
+                Console.Write(" " + fibonacci);
+            }
+            return 0;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("        Welcome to Logical Programs        ");
+
+            while (true)
+            {
+                Console.WriteLine("Press 1. for Fibonacci Series.");
+                Console.WriteLine("Press 2. for Perfect number.");
+                Console.WriteLine("Press 3. for Prime number.");
+                Console.WriteLine("Press 4. for Reverse number.");
+                Console.WriteLine("Press 5. to Stimulate Stop watch.");
+                Console.WriteLine("|--------------------------------------------------|");
+                Console.WriteLine("| Press 1. for Fibonacci Series.                   |");
+                Console.WriteLine("| Press 2. for Perfect Number.                     |");
+                Console.WriteLine("| Press 3. for Prime Number.                       |");
+                Console.WriteLine("| Press 4. for Reverse Number.                     |");
+                Console.WriteLine("| Press 5. for Stimulate Stop Watch.               |");
+                Console.WriteLine("| Press 6. To Exit.                                |");
+                Console.WriteLine("|--------------------------------------------------|");
+                Console.WriteLine(" Enter your choice: ");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        //Fibonacci Series
+                        FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                        fibonacciSeries.Fibonacciseries(10);
+                        break;
+
+                   /* case 2:
+                        //perfect number (6, 28, 496, 8128 )
+                        PerfectNumber perfect = new PerfectNumber();
+                        perfect.CheckPerfectNumber(8128);
+                        break;
+
+                    case 3:
+                        //prime number (2,3,5,7,11... )
+                        PrimeNumber prime = new PrimeNumber();
+                        prime.CheckPrime(23);
+                        break;
+
+                    case 4:
+                        ////Reverse a number
+                        ReverseNumebr reverse = new ReverseNumebr();
+                        reverse.DoReverse(123);
+                        break;
+
+                    case 5:
+                        //Stopwatch
+                        StopWatch watch = new StopWatch();
+                        watch.StimulateStopWatch();
+                        break; */
+
+                    case 6:
+                        Console.WriteLine("Exit");
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("invalid selection made.");
+                        break;
+
+                }
+            }
+        }
+    }
+}
